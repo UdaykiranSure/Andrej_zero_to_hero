e827159f2d41cf1c8a7d9624b7ea57bbb0b0f754
UdayKiranSure <sureudaykiran1@gmail.com>
2026-01-12 21:38:44 +0530

    implemented model class and sinosual positional encoding

--- gpt-2.py
+++ gpt-2.py
@@ -55,3 +55,126 @@ class FFN(nn.Module):
         out = self.l2(out)
 
         return out
+
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model, d_k, d_v, vocab_size, n_head):
+        super(DecoderBlock, self).__init__()
+
+        self.self_att = MultiHeadAttention(d_model, d_k, d_v, n_head, masked= True)
+        self.cross_att = MultiHeadAttention(d_model, d_k, d_v, n_head)
+        self.ffn = FFN(d_model)
+        self.ln = nn.LayerNorm(d_model)
+
+    def forward(self, x, enc_out):
+        """
+        params: 
+        x: positional encoded output token sequence. #(seq_len, d_model)
+        enc_out: encoder output hidden represenations of input sequence
+
+        outputs:
+        out: encoder output representation of input sequence  #(seq_len, d_model)
+        """
+
+        out = self.self_att(x,x,x)
+        att1_out = self.ln(x + out)
+        att2_out = self.cross_att(att1_out, enc_out, enc_out)
+        out = self.ln(att2_out + att1_out)
+        out = self.ln(self.ffn(out) + out)
+
+        return out
+
+# d_model = 32
+# d_k = d_v = 16
+# n_heads = 4
+# seq_len = 3
+# vocab_size = 200
+# enc = EncoderBlock(d_model,d_k, d_v, vocab_size, n_heads)
+
+# x = torch.rand(4,seq_len,d_model)
+# enc_out = enc(x)
+
+# dec  = DecoderBlock(d_model, d_k, d_v, vocab_size, n_heads)
+# out = dec(x,enc_out)
+# out.shape
+
+
+class Model(nn.Module):
+    def __init__(self, d_model, d_k, d_v, vocab_size, n_head, n_layers = 1, ):
+        """
+        d_modle: dimensional size of hidden representations
+        d_k:  dimentional size of key and query projection matrix
+        d_v: dimentional size of value projection matrix
+        vocab_size: size of vocabulary in the training data
+        """
+
+        super(Model,self).__init__()
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.vocab_size = vocab_size
+
+        # Initialise input embedding matrix
+        self.tok_embeddings = nn.Embedding(vocab_size, d_model)
+        
+        self.enc_blocks = nn.ModuleList([EncoderBlock(d_model, d_k, d_v, vocab_size, n_head) for _ in range(n_layers)])
+        self.dec_blocks = nn.ModuleList([DecoderBlock(d_model, d_k, d_v, vocab_size, n_head) for _ in range(n_layers)])
+
+        # model ouput linear layer
+        self.model_out = nn.Linear(d_model, vocab_size)
+
+
+
+    def _positional_encode(self, seq_len,d_model):
+        """
+        TODO: can this be cached while initialising the model, so don't need to calculate at every forward pass
+
+        params:
+        inp_len : number of tokens in the input
+        d_model: dimension of hidden layer
+
+        ouputs:
+        pe : postional vector of size (d_model, seq_len)
+
+        PE(pos, 2i) = sin( pos / 10000^(2*i/d_model) )
+        PE(pos, 2i + 1) = cos( pos / 10000(2*i/d_model) )
+        """
+        pos_embs = torch.zeros(seq_len, d_model)
+        for pos in torch.arange(seq_len):
+            for i in torch.arange(0, d_model, 2):
+                pos_embs[pos, i]     = torch.sin( pos / 10000** ((2*i)/d_model))
+                pos_embs[pos, i + 1] = torch.cos( pos/ 10000** ((2*i)/d_model))
+        return pos_embs
+
+
+
+    def forward(self, inputs, outputs=None):
+        
+
+
+
+# d_model = 32
+# d_k = d_v = 16
+# n_head = 4
+# seq_len = 3
+# vocab_size = 200
+
+# inputs = torch.randint(0,50,(10,5))
+# trs = Model(d_model, d_k, d_v, vocab_size, n_head)
+# out = trs(inputs)
+# out.shape
+# out.shape
+# pos = torch.ones(2,5)
+# torch.sin(pos)
+
+# embs = torch.nn.Embedding(25,8)
+# inputs = torch.zeros((10,1),dtype=torch.int)
+# embs(inputs).shape
+
+# pos_embs = torch.ones((seq_len,d_model))
+# for pos in range(0,seq_len-1):
+#     for i in range(0, d_model, 2):
+#         pos_embs[pos, i]     = torch.sin( pos / 10000 ** ((2*i)/d_model))
+#         pos_embs[pos, i + 1] = torch.cos( pos/ 10000** ((2*i)/d_model))
+
