2e2695d2e0e405dded23473446313b9dbbc5d6c9
UdayKiranSure <sureudaykiran1@gmail.com>
2026-01-18 19:07:54 +0530

    completed gpt architecture

--- gpt-2.py
+++ gpt-2.py
@@ -3,41 +3,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class MultiHeadAttention(nn.Module):
+class CasualSelfAttention(nn.Module):
     def __init__(self, d_model, d_k, d_v, n_head, masked = False):
 
         assert d_k%n_head == 0, "attention key matrix dimenstion should be multiple of num of heads"
         assert d_v%n_head == 0, "attention value matrix dimenstion should be multiple of num of heads"
 
-        super(MultiHeadAttention, self).__init__()
+        super(CasualSelfAttention, self).__init__()
         self.d_model = d_model
         self.d_k = d_k
         self.d_v = d_v
         self.n_head = n_head
+
         self.query = nn.Linear(d_model,d_k, bias = False)
         self.key = nn.Linear(d_model,d_k, bias = False)
         self.value = nn.Linear(d_model,d_v, bias = False)
+
         self.proj = nn.Linear(d_v, d_model, bias= False)
-        self.masked = masked
+        self.proj.NANOGPT_SCALE_INIT = 1
 
         # if masked:
         #     self.register_buffer('mask', torch.tril(torch.ones(seq_len,seq_len)).view(1,1,seq_len,seq_len))
 
-    def forward(self, q, k, v):
-        B, out_seq_len, d_model = q.shape
-        B, in_seq_len,  d_model = k.shape
-        Q = self.query(q).view(B,self.n_head, out_seq_len,self.d_k//self.n_head)       #(B, n_heads, seq_len, d_k//n_heads)
-        K = self.key(k).view(B,self.n_head, in_seq_len,self.d_k//self.n_head)         #(B, n_heads, seq_len, d_k//n_heads)
-        V = self.value(v).view(B,self.n_head, in_seq_len,self.d_v//self.n_head)       #(B, n_heads, seq_len, d_v//n_heads)
-        
-        wei = (Q @ (K.transpose(-1, -2)))                         #(B, n_heads, seq_len, seq_len)
-        if self.masked:
-            mask = torch.tril(torch.ones(out_seq_len,in_seq_len)).view(1,1,out_seq_len,in_seq_len)
-            wei = torch.masked_fill(wei,mask == 0,-torch.inf)
-        att = F.softmax(wei,3) @ V                                # (B, n_heads, seq_len, d_v//n_heads)
-        out = att.transpose(1,2).reshape(B , out_seq_len, d_v)        # (B, seq_len, d_v)
-        out = self.proj(out)                                      # (B, seq_len, d_model)
+    def forward(self, x):
+        B, seq_len, d_model = q.shape
+        q = self.query(x).view(B,self.n_head, seq_len,self.d_k//self.n_head)       #(B, n_heads, seq_len, d_k//n_heads)
+        k = self.key(x).view(B,self.n_head, seq_len,self.d_k//self.n_head)         #(B, n_heads, seq_len, d_k//n_heads)
+        v = self.value(x).view(B,self.n_head, seq_len,self.d_v//self.n_head)       #(B, n_heads, seq_len, d_v//n_heads)
 
+        # Scratch implementaion
+        # wei = (q @ (k.transpose(-1, -2)))                         #(B, n_heads, seq_len, seq_len)
+        # if self.masked:
+        #     mask = torch.tril(torch.ones(seq_len, seq_len)).view(1,1,seq_len,seq_len)
+        #     wei = torch.masked_fill(wei,mask == 0,-torch.inf)
+        # att = F.softmax(wei,3) @ v                              # (B, n_heads, seq_len, d_v//n_heads)
+
+        # Pytorch's Flash attention
+        out = F.scaled_dot_product_attention(q,k,v, is_causal=True)
+
+        out = out.transpose(1,2).reshape(B , seq_len, d_v)        # (B, seq_len, d_v)
+        out = self.proj(out)                                      # (B, seq_len, d_model)
         return out
 
 
@@ -45,27 +50,27 @@ class FFN(nn.Module):
     def __init__(self, d_model):
         super(FFN, self).__init__()
         self.d_model = d_model
-        self.l1 = nn.Linear(d_model, d_model)
-        self.l2 = nn.Linear(d_model, d_model)
-        self.relu = nn.ReLU()
+        self.l1 = nn.Linear(d_model, 4*d_model)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.l2 = nn.Linear(d_model, 4*d_model)
+        self.l2.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-
-        return out
-
+        x = self.l1(x)
+        x = self.gelu(x)
+        x = self.l2(x)
+        return x
 
 
-class DecoderBlock(nn.Module):
+
+class Block(nn.Module):
     def __init__(self, d_model, d_k, d_v, vocab_size, n_head):
         super(DecoderBlock, self).__init__()
-
-        self.self_att = MultiHeadAttention(d_model, d_k, d_v, n_head, masked= True)
-        self.cross_att = MultiHeadAttention(d_model, d_k, d_v, n_head)
+        
+        self.ln1 = nn.LayerNorm(d_model)
+        self.attn = CasualSelfAttention(d_model, d_k, d_v, n_head, masked= True)
+        self.ln2 = nn.LayerNorm(d_model)
         self.ffn = FFN(d_model)
-        self.ln = nn.LayerNorm(d_model)
 
     def forward(self, x, enc_out):
         """
@@ -77,31 +82,25 @@ class DecoderBlock(nn.Module):
         out: encoder output representation of input sequence  #(seq_len, d_model)
         """
 
-        out = self.self_att(x,x,x)
-        att1_out = self.ln(x + out)
-        att2_out = self.cross_att(att1_out, enc_out, enc_out)
-        out = self.ln(att2_out + att1_out)
-        out = self.ln(self.ffn(out) + out)
-
-        return out
-
-# d_model = 32
-# d_k = d_v = 16
-# n_heads = 4
-# seq_len = 3
-# vocab_size = 200
-# enc = EncoderBlock(d_model,d_k, d_v, vocab_size, n_heads)
-
-# x = torch.rand(4,seq_len,d_model)
-# enc_out = enc(x)
-
-# dec  = DecoderBlock(d_model, d_k, d_v, vocab_size, n_heads)
-# out = dec(x,enc_out)
-# out.shape
+        x = x + self.attn(self.ln1(x))
+        x = x + self.ffn(self.ln2(x))
+        return x
 
 
-class Model(nn.Module):
-    def __init__(self, d_model, d_k, d_v, vocab_size, n_head, n_layers = 1, ):
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    d_model: int = 768 # embedding dimension
+    d_q: int = 768 # embedding dimension
+    d_k: int = 768 # embedding dimension
+    d_v: int = 768 # embedding dimension
+    
+
+class GPT(nn.Module):
+    def __init__(self, d_model, d_q, d_k, d_v, vocab_size, n_head, block_size, n_layers = 1 ):
         """
         d_modle: dimensional size of hidden representations
         d_k:  dimentional size of key and query projection matrix
@@ -113,44 +112,69 @@ class Model(nn.Module):
         self.d_model = d_model
         self.d_k = d_k
         self.d_v = d_v
+        self.d_q = d_q
         self.vocab_size = vocab_size
-
-        # Initialise input embedding matrix
-        self.tok_embeddings = nn.Embedding(vocab_size, d_model)
+        self.block_size = block_size
+        self.n_layers = n_layers
         
-        self.enc_blocks = nn.ModuleList([EncoderBlock(d_model, d_k, d_v, vocab_size, n_head) for _ in range(n_layers)])
-        self.dec_blocks = nn.ModuleList([DecoderBlock(d_model, d_k, d_v, vocab_size, n_head) for _ in range(n_layers)])
+        self.transfomer = nn.ModuelDict(dict(
+            wte = nn.Embedding(self.vocab_size, self.d_model),
+            wpe = nn.Embedding(self.vocab_size, self.d_model),
+            h = nn.ModuleList([Block(d_model, d_k, d_v, vocab_size, n_head) for _ in range(n_layers)]),
+            ln_f = nn.LayerNorm(d_model)
+        ))
 
         # model ouput linear layer
-        self.model_out = nn.Linear(d_model, vocab_size)
+        self.lm_head = nn.Linear(d_model, vocab_size)
+
+        # weight sharing scheme
+        self.transfomer.wte.weight = self.lm_head.weight
+
+        #initialise parameters
+        self.apply(self._init_weights)
+
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.n_layers)** -0.5 
+            torch.nn.init.normal_(module.weight ,mean=0.0, std = std)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+    def forward(self, inputs, targets= None):
+        B, seq_len = inputs.shape
+
+        assert seq_len == self.block_size, f"Cannot forward sequence of length {T}, block size is only {self.block_size}"
+
+        pos = torch.arange(0, seq_len, dtype=torch.long, device = inputs.device)
+        pos_emb = self.transfomer.wpe(pos)
+        tok_emb = self.transfomer.wte(inputs)
+        x = tok_emb + pos_emb
+        for block in self.transfomer.h:
+            x = block(x)
+        x = self.transfomer.ln_f(x)
+
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
 
 
-    def _positional_encode(self, seq_len,d_model):
-        """
-        TODO: can this be cached while initialising the model, so don't need to calculate at every forward pass
-
-        params:
-        inp_len : number of tokens in the input
-        d_model: dimension of hidden layer
-
-        ouputs:
-        pe : postional vector of size (d_model, seq_len)
-
-        PE(pos, 2i) = sin( pos / 10000^(2*i/d_model) )
-        PE(pos, 2i + 1) = cos( pos / 10000(2*i/d_model) )
-        """
-        pos_embs = torch.zeros(seq_len, d_model)
-        for pos in torch.arange(seq_len):
-            for i in torch.arange(0, d_model, 2):
-                pos_embs[pos, i]     = torch.sin( pos / 10000** ((2*i)/d_model))
-                pos_embs[pos, i + 1] = torch.cos( pos/ 10000** ((2*i)/d_model))
-        return pos_embs
 
 
 
-    def forward(self, inputs, outputs=None):
-        
+
+
+
+
+
 
 
 
